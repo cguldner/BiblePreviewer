989d45f5ec9859e9fa2ff979fa157773acc16a85
Cody <codyguldner2@gmail.com>
2018-12-27 17:57:48 -0600

    Fixed the popup referencing the wrong JS file, and not including the URL when it sends a message

--- js/popup.js
+++ js/popup.js
@@ -1,6 +1,7 @@
 document.getElementById('reload-button').addEventListener('click', function () {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.storage.sync.get(null, function (settings) {
+            settings['url'] = tabs[0].url;
             chrome.tabs.sendMessage(tabs[0].id, settings, function (response) {
                 // Make the popup close after clicking on the button
                 window.close();
